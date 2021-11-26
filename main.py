2e0b51162b3d7dc441d427bb78c27f9a43006ab8
Aaron Bruce <aaronbruce555@gmail.com>
2021-11-26 14:01:04 -0500

    Add some prompt functions for gathering user input

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
 import selenium
+from services.promptAction import promptAction
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 # import time
 # from PIL import Image
-# import io
+import io
 # import requests
+
+# global variables
+url = "https://apps.irs.gov/app/picklist/list/priorFormPublication.html"
+
+# input state
+prompt = True
+action = None
 
 # Taking a list of tax form names (ex: "Form W-2", "Form 1095-C"), 
 # search the website and return some informational results. 
@@ -22,14 +30,34 @@ from selenium.webdriver.common.by import By
 # the Product Name, the Title, and max - min years of the form availability.
 # return all instances as a list of dicts containing the key value pairs of the aforementioned info
 
+def searchByFormName(forms):
+    driver = webdriver.Firefox()
+    print("I'm the searchByFormName method!")
+    driver.get(url)
+    print(f"Driver {driver}")
+    driver.close()
 
-driver = webdriver.Firefox()
-driver.get("https://apps.irs.gov/app/picklist/list/priorFormPublication.html")
-print(f"Driver {driver}")
+def getFormNames():
+    enter = True
+    form_names = []
+    while enter:
+        form_name = input('Enter Form Name: ')
+        again = input('Press N if you are done, otherwise any key to keep adding form names: ')
+        form_names.append(form_name)
+        if again == 'n' or again == 'N': enter = False
+    return form_names
+
+# Prompt the user for an action, f/F denotes to searchByFormName
+action = promptAction(prompt)
+if action == 'F':
+    form_names = getFormNames()
+    searchByFormName(form_names)
+    
+
 # assert "IRS" in driver.title
 # elem = driver.find_element()
 # elem.clear()
 # elem.send_keys("pycon")
 # elem.send_keys(Keys.RETURN)
 # assert "No results found." not in driver.page_source
-driver.close()
+# driver.close()
