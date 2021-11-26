6b7055e15616cc316b40f1712f98d47c8e2ff587
Aaron Bruce <aaronbruce555@gmail.com>
2021-11-26 13:17:13 -0500

    Update README with some version info and main.py with some task comments

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+# Versions
+Python v3.9.7
+geckodriver v0.30.0-linux32.tar.gz unpacked to ./venv/bin
+
 # Steps to Setup
 
-- Setup venv and source into it
-- Add geckodriver to your venv/bin/
+- Setup venv and source into it, I created it within the root of this app
+- Add geckodriver to your ./venv/bin/
 - pip install -r requirements.txt
--- main.py
+++ main.py
@@ -8,6 +8,21 @@ from selenium.webdriver.common.by import By
 # import io
 # import requests
 
+# Taking a list of tax form names (ex: "Form W-2", "Form 1095-C"), 
+# search the website and return some informational results. 
+# Specifically, you must return the "Product Number", the "Title", and the maximum and minimum years the form is available for download. 
+# The forms returned should be an exact match for the input (ex: "Form W-2" should not return "Form W-2 P", etc.)
+
+# Upon start of the script, prompt the user for the desired actions to take
+# Error handle incorrect entries, there will be 2 options to take.
+# Upon selection of the action, prompt for arguements to use for the function.
+# Error handle 404 responses when nothing is returned
+
+# Declare a function that takes the Form Name and search through the website to find
+# the Product Name, the Title, and max - min years of the form availability.
+# return all instances as a list of dicts containing the key value pairs of the aforementioned info
+
+
 driver = webdriver.Firefox()
 driver.get("https://apps.irs.gov/app/picklist/list/priorFormPublication.html")
 print(f"Driver {driver}")
