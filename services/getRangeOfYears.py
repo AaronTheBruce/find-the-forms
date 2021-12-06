ecc2c797a8dd136f22f525c8fed71acabb64f47b
Aaron Bruce <aaronbruce555@gmail.com>
2021-12-05 19:34:01 -0500

    Add Download IRS PDF functionality

---
+++ services/getRangeOfYears.py
@@ -0,0 +1,7 @@
+def getRangeOfYears(f, l):
+    arr = []
+    index = f
+    while index <= l:
+        arr.append(index)
+        index += 1
+    return arr
--- services/promptAction.py
+++ services/promptAction.py
@@ -1,9 +1,13 @@
 def promptAction(prompt):
     while prompt == True:
         print("Search by Form Name(s): Press F")
+        print("Download Forms by earliest to latest year: Press D")
         action = input("What Action do you want to take?: ")
         if action == 'F' or action == 'f':
             prompt = False
             return 'F'
+        elif action == 'D' or action == 'd':
+          prompt = False
+          return 'D'  
         else:
             print("Invalid Input")
